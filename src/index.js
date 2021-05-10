351990c87c3b75f9e2810c001141f66bc295f58c
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-05-10 20:05:45 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+ 
+import { createStore } from './store';
+ 
+console.log(store);
 
-function render(){
-  ReactDOM.render(<InsertTopLevelComponent />, document.getElementById('container'))
+import BandInput from './components/band_input_component';
+import bandReducer from './reducers/band_reducer.js';
+ 
+const store = createStore(bandReducer);
+
+ReactDOM.render(<BandInput store={store}/>, document.getElementById('container'))
+
+const renderApp = () => {
+  ReactDOM.render(<BandInput store={store}/>, document.getElementById('container'))
 }
+
+store.subscribe(renderApp);
+store.dispatch({})
